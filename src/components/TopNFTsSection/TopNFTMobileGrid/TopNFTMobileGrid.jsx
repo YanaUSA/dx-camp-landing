191e82ab4a0bdcb5ec3fa3d4a3a34492bab16764
YanaUSA <usatiukiana@gmail.com>
2023-09-05 22:52:24 +0300

    fixed aria labels

--- src/components/TopNFTsSection/TopNFTMobileGrid/TopNFTMobileGrid.jsx
+++ src/components/TopNFTsSection/TopNFTMobileGrid/TopNFTMobileGrid.jsx
@@ -7,24 +7,24 @@ const TopNFTMobileGrid = ({ data }) => {
         <li key={id} className={styles.listWrapper}>
           <div className={styles.mobNFTtable} role="table" aria-label="Top NFTs users table" aria-describedby="users_table_description">
             <div className={styles.rarityLevel} role="rowgroup">
-              <p className={styles.textStyle} role="cell">Rarity:</p>
+              <p className={styles.textStyle}>Rarity:</p>
             </div>
             <div className={styles.totalGames} role="rowgroup">
-              <p className={styles.textStyle} role="cell">Total Games:</p>
+              <p className={styles.textStyle}>Total Games:</p>
             </div>
             <div className={styles.gamesWon} role="rowgroup">
-              <p className={styles.textStyle} role="cell">Games Won:</p>
+              <p className={styles.textStyle}>Games Won:</p>
             </div>
             <div className={styles.rarityLevelValue} role="row">
-              <p className={styles.textStyle} role="cell">{item.rarity}</p>
+              <p className={styles.textStyle}>{item.rarity}</p>
             </div>
             <div className={styles.totalGamesValue} role="row">
-              <p className={styles.textStyle} role="cell">{item.totalGames}</p>
+              <p className={styles.textStyle}>{item.totalGames}</p>
             </div>
             <div className={styles.gamesWonValue} role="row">
-              <p className={styles.textStyle} role="cell">{item.gamesWon}</p>
+              <p className={styles.textStyle}>{item.gamesWon}</p>
             </div>
-            <div className={styles.NFTuserAvatar} role="cell">
+            <div className={styles.NFTuserAvatar}>
               <img
                 src={item.avatar}
                 alt="NFT user avatar"
@@ -32,10 +32,10 @@ const TopNFTMobileGrid = ({ data }) => {
               />
             </div>
             <div className={styles.NFTName} role="row">
-              <p className={styles.boldTextStyle} role="cell">{item.name}</p>
+              <p className={styles.boldTextStyle}>{item.name}</p>
             </div>
             <div className={styles.Price} role="row">
-              <p className={styles.boldTextStyle} role="cell">{item.price}ETH</p>
+              <p className={styles.boldTextStyle}>{item.price}ETH</p>
             </div>
           </div>
         </li>
