664b1d344fa7a6c0a39055750975d407a40ea666
YanaUSA <usatiukiana@gmail.com>
2023-09-06 09:22:13 +0300

    fix

--- src/components/FeaturesCard/cards.js
+++ src/components/FeaturesCard/cards.js
@@ -3,12 +3,12 @@ export const cards = [
     id: "01",
     title: "STRU Token sale",
     imageMob:
-      "@/assets/assets/images/2-1-pc.jpg 1x, @/assets/images/2-1-pc@2x.jpg 2x",
+      "../assets/assets/images/2-1-pc.jpg 1x, ../assets/images/2-1-pc..2x.jpg 2x",
     imageTab:
-      "@/assets/images/2-1-tab.jpg 1x, @/assets/images/2-1-tab@2x.jpg 2x",
+      "../assets/images/2-1-tab.jpg 1x, ../assets/images/2-1-tab..2x.jpg 2x",
     imageDesc:
-      "@/assets/images/2-1-pc.jpg 1x, @/assets/images/2-1-pc@2x.jpg 2x",
-    imageDefault: "@/assets/images/2-1-pc.jpg",
+      "../assets/images/2-1-pc.jpg 1x, ../assets/images/2-1-pc..2x.jpg 2x",
+    imageDefault: "../assets/images/2-1-pc.jpg",
     image_alt: "Futuristic astronaut travels through space",
     mediaMob: "(min-width: 375px)",
     mediaTab: "(min-width: 744px)",
@@ -20,12 +20,12 @@ export const cards = [
     id: "02",
     title: "Staking",
     imageMob:
-      "@/assets/images/2-2-pc.jpg 1x, @/assets/images/2-2-pc@2x.jpg 2x",
+      "../assets/images/2-2-pc.jpg 1x, ../assets/images/2-2-pc..2x.jpg 2x",
     imageTab:
-      "@/assets/images/2-2-tab.jpg 1x, @/assets/images/2-2-tab@2x.jpg 2x",
+      "../assets/images/2-2-tab.jpg 1x, ../assets/images/2-2-tab..2x.jpg 2x",
     imageDesc:
-      "@/assets/images/2-2-pc.jpg 1x, @/assets/images/2-2-pc@2x.jpg 2x",
-    imageDefault: "@/assets/images/2-2-pc.jpg",
+      "../assets/images/2-2-pc.jpg 1x, ../assets/images/2-2-pc..2x.jpg 2x",
+    imageDefault: "../assets/images/2-2-pc.jpg",
     image_alt: "Futuristic astronaut travels through space",
     mediaMob: "(min-width: 375px)",
     mediaTab: "(min-width: 744px)",
@@ -37,12 +37,12 @@ export const cards = [
     id: "03",
     title: "NFT minting",
     imageMob:
-      "@/assets/images/2-3-pc.jpg 1x, @/assets/images/2-3-pc@2x.jpg 2x",
+      "../assets/images/2-3-pc.jpg 1x, ../assets/images/2-3-pc..2x.jpg 2x",
     imageTab:
-      "@/assets/images/2-3-tab.jpg 1x, @/assets/images/2-3-tab@2x.jpg 2x",
+      "../assets/images/2-3-tab.jpg 1x, ../assets/images/2-3-tab..2x.jpg 2x",
     imageDesc:
-      "@/assets/images/2-3-pc.jpg 1x, @/assets/images/2-3-pc@2x.jpg 2x",
-    imageDefault: "@/assets/images/2-3-pc.jpg",
+      "../assets/images/2-3-pc.jpg 1x, ../assets/images/2-3-pc..2x.jpg 2x",
+    imageDefault: "../assets/images/2-3-pc.jpg",
     image_alt: "Futuristic astronaut travels through space",
     mediaMob: "(min-width: 375px)",
     mediaTab: "(min-width: 744px)",
