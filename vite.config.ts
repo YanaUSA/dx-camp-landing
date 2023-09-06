ab4b2333ac97ff7465f15b4eea19dee80ea48e1a
YanaUSA <usatiukiana@gmail.com>
2023-09-06 08:58:51 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+// import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: {
+    alias: [{
       // '@': path.resolve(__dirname, './src'),
       // '@assets': path.resolve(__dirname, './src/assets'),
       // '@components': path.resolve(__dirname, './src/components'),
-      src: path.resolve('src/'),
-    },
+      find: '@', replacement: '/src',
+    }],
   },
   plugins: [react()],
 })
